09f04f5727bce8474eda88bcb7df4a68c31ff263
joeban <joeanddaneil5@gmail.com>
2026-04-23 20:36:56 +0800

    - remove vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { resolve } from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    lib: {
-      // 這是你的組件進入點
-      entry: resolve(__dirname, "src/share-ui/Button.tsx"),
-      name: "MyShareUI",
-      // 指定輸出的檔名，這樣就不會有隨機的 hash 碼
-      fileName: (format) => `index.${format}.js`,
-      formats: ["es"], // 使用 ES Module
-    },
-    rollupOptions: {
-      // 確保不要把 React 打包進去，由 main-app 提供
-      external: ["react", "react-dom", "react/jsx-runtime"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-    },
-  },
 });
